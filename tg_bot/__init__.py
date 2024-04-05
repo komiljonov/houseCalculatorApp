674106279d9621cc578a92bb963eaec20e0330e5
komiljonov <komiljonovshukurullokh@gmail.com>
2024-04-05 14:53:41 +0500

    dfg

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from tkinter.messagebox import QUESTION
 
 from django.core.files.base import File
-from telegram import Bot, Update,Message
+from telegram import Bot, Update, Message
 from telegram.ext import (
     ApplicationBuilder,
     CallbackContext,
@@ -33,18 +33,20 @@ class Bot:
                 ],
                 {
                     MENU: [
-                        MessageHandler(filters.Text(["Kalkulator"]), self.calculator),
-                        MessageHandler(filters.Text(["Savol berish"]), self.question)
+                        MessageHandler(filters.Text(
+                            ["Kalkulator"]), self.calculator),
+                        MessageHandler(filters.Text(
+                            ["Savol berish"]), self.question)
                     ],
                     HOME_PRICE: [
                         MessageHandler(filters.Regex(
                             r"^\d+$"), self.home_price),
-                            MessageHandler(filters.Text([BACK]),self.start)
+                        MessageHandler(filters.Text([BACK]), self.start)
                     ],
                     STARTUP_PRICE: [
                         MessageHandler(filters.Regex(
                             r"^\d+$"), self.startup_price),
-                            MessageHandler(filters.Text([BACK]),self.calculator)
+                        MessageHandler(filters.Text([BACK]), self.calculator)
 
                     ],
                     # PERCENT_PER_YEAR: [
@@ -60,22 +62,24 @@ class Bot:
                     #     MessageHandler(filters.Text([BACK]),self.back_from_credit_duration)
                     # ],
                     QUESTION: [
-                        MessageHandler(filters.TEXT & EXCLUDE, self.question_text)
+                        MessageHandler(filters.TEXT & EXCLUDE,
+                                       self.question_text)
                     ]
                 },
                 [
-                    CommandHandler('start',self.start)
+                    CommandHandler('start', self.start)
                 ],
                 name="MainConversation"
             )
         )
 
-
         self.app.add_handler(
-            MessageHandler(filters.ChatType.GROUP & filters.REPLY & filters.TEXT & EXCLUDE,self.group_question_answer)
+            MessageHandler(filters.ChatType.GROUP & filters.REPLY &
+                           filters.TEXT & EXCLUDE, self.group_question_answer)
         )
         self.app.add_handler(
-            MessageHandler(filters.ChatType.PRIVATE & filters.REPLY & filters.TEXT & EXCLUDE,self.group_question_answer_user)
+            MessageHandler(filters.ChatType.PRIVATE & filters.REPLY &
+                           filters.TEXT & EXCLUDE, self.group_question_answer_user)
         )
 
         self.app.run_polling()
@@ -94,6 +98,9 @@ class Bot:
 
     async def calculator(self, update: Update, context: CallbackContext):
         tgUser, user, temp = User.get(update)
+
+        await tgUser.send_message("Siz `Yangi Hayot` uylarini 18% bilan,\n"
+                                  "20 yil kridet muddatiga olishingiz mumkun!")
 
         await tgUser.send_message("Olinayotgan uy narxini kiriting.")
         return HOME_PRICE
@@ -114,10 +121,6 @@ class Bot:
         temp.credit_duration = 20
         temp.save()
 
-
-
-
-
         b = temp.home_price - temp.startup_price
 
         z = temp.percent_per_year / 100
@@ -128,9 +131,6 @@ class Bot:
 
         await tgUser.send_message(f"Oylik to'lov: {res:,}")
         return await self.start(update, context)
-
-
-
 
     # async def percent_per_year(self, update: Update, context: CallbackContext):
     #     tgUser, user, temp = User.get(update)
@@ -158,18 +158,10 @@ class Bot:
     #     await tgUser.send_message(f"Oylik to'lov: {res:,}")
     #     return await self.start(update, context)
 
-
-
-
-
-
     # async def back_from_percent_per_year(self,update:Update,context:CallbackContext):
     #     tgUser, user, temp = User.get(update)
     #     await tgUser.send_message("Boshlang'ich to'lovni kiriting.")
     #     return STARTUP_PRICE
-
-
-
 
     # async def back_from_credit_duration(self,update:Update,context:CallbackContext):
     #     tgUser, user, temp = User.get(update)
@@ -177,26 +169,17 @@ class Bot:
 
     #     return PERCENT_PER_YEAR
 
-
-
-
-
-
-    async def question(self,update:Update,context:CallbackContext):
+    async def question(self, update: Update, context: CallbackContext):
         tgUser, user, temp = User.get(update)
 
-
-        await tgUser.send_message("Iltimos savolingizni yuboring.",reply_markup=ReplyKeyboardMarkup())
+        await tgUser.send_message("Iltimos savolingizni yuboring.", reply_markup=ReplyKeyboardMarkup())
 
         return QUESTION
 
-
-    async def question_text(self,update:Update,context:CallbackContext):
+    async def question_text(self, update: Update, context: CallbackContext):
         tgUser, user, temp = User.get(update)
 
         text = update.message.text
-
-
 
         group_message: Message = await update.get_bot().send_message(
             -4162149949,
@@ -208,8 +191,6 @@ class Bot:
 
         )
 
-
-
         new_question = Question.objects.create(
             user=user,
             message=text,
@@ -219,27 +200,22 @@ class Bot:
 
         await tgUser.send_message("Sizning savolingiz yuborildi.\n\nIltimos javob berishlarini kuting.")
 
-        return await self.start(update,context)
+        return await self.start(update, context)
 
-    async def group_question_answer(self,update:Update,context:CallbackContext):
+    async def group_question_answer(self, update: Update, context: CallbackContext):
         tgUser, user, temp = User.get(update)
 
-
-
-        question = Question.objects.filter(group_message_id=update.message.reply_to_message.message_id).first()
+        question = Question.objects.filter(
+            group_message_id=update.message.reply_to_message.message_id).first()
 
         if not question:
             return
-
-
 
         user_message: Message = (await update.get_bot().send_message(
             question.user.chat_id,
             update.message.text,
             reply_to_message_id=question.user_message_id
         ))
-
-
 
         new_question = Question.objects.create(
             user=user,
@@ -249,20 +225,14 @@ class Bot:
             sender=2
         )
 
-
-
-
-    async def group_question_answer_user(self,update:Update,context:CallbackContext):
+    async def group_question_answer_user(self, update: Update, context: CallbackContext):
         tgUser, user, temp = User.get(update)
 
-
-
-        question = Question.objects.filter(user_message_id=update.message.reply_to_message.message_id).first()
+        question = Question.objects.filter(
+            user_message_id=update.message.reply_to_message.message_id).first()
 
         if not question:
             return
-
-
 
         group_message: Message = (await update.get_bot().send_message(
             -4162149949,
@@ -274,8 +244,6 @@ class Bot:
             reply_to_message_id=question.group_message_id
         ))
 
-
-
         new_question = Question.objects.create(
             user=user,
             message=update.message.text,
@@ -284,4 +252,3 @@ class Bot:
             user_message_id=update.message.message_id,
             group_message_id=group_message.message_id
         )
-
